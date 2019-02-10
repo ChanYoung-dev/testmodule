71cf7974d9affeccec764dd2ddbc79b5eac86e7f
kimchanyoung <emrhs1004@naver.com>
2019-02-11 01:07:09 +0900

    start&getDOM

--- MMM-Test.js
+++ MMM-Test.js
@@ -2,11 +2,21 @@ Module.register("MMM-Test", {
     defaults: {
             foo: "I'm alive!"
     },
-    start: function () {},
+    start: function () {
+        this.count = 0
+        var timer = setInterval(()=>{
+            this.updateDom()
+            this.count++
+        }, 1000)
+    },
     getDom: function() {
         var element = document.createElement("div")
         element.className = "myContent"
-        element.innerHTML = "Hello, World!" + this.config.foo
+        element.innerHTML = "Hello, World! " + this.config.foo
+        var subElement = document.createElement("p")
+        subElement.innerHTML = "Count:" + this.count
+        subElement.id = "COUNT"
+        element.appendChild(subElement)
         return element
     },
     notificationReceived: function() {},
