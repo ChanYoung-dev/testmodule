0b59dcf41136eb1e3f5f9e3b7b71f542bdd40e9e
kimchanyoung <emrhs1004@naver.com>
2019-02-18 14:21:54 +0900

    error

--- MMM-Test.js
+++ MMM-Test.js
@@ -1,36 +1,36 @@
 Module.register("MMM-Test", {
-    defaults: {
-            foo: "I'm alive!"
-    },
-    start: function () {
-        this.count = 0
-    },
-    getDom: function() {
-        var element = document.createElement("div")
-        element.className = "myContent"
-        element.innerHTML = "Hello, World! " + this.config.foo
-        var subElement = document.createElement("p")
-        subElement.innerHTML = "Count:" + this.count
-        subElement.id = "COUNT"
-        element.appendChild(subElement)
-        return element
-    },
-    notificationReceived: function(notification, payload, sender) {
-        switch(notification){
-            case "DOM_OBJECTS_CREATED":
-                var timer = setInterval(()=>{
-                    var countElm = document.getElementById("COUNT")
-                    countElm.innerHTML= "Count:" + this.count
-                    this.count++
-                    /*
-                    this.updateDom()
-                    this.count++
-                    */
-                }, 1000)
-                break
-            }
-    },
-    
-    
-    socketNotificationReceived: function() {},
-  })
+  defaults: {},
+  start: function (){},
+
+getDom: function() {
+  var element = document.createElement("div")
+  element.className = "myContent"
+  element.innerHTML = "Hello, World! " + this.config.foo
+  var subElement = document.createElement("p")
+  subElement.innerHTML = "Test" 
+  subElement.id = "TEST"
+  element.appendChild(subElement)
+  return element
+},
+
+
+notificationReceived: function(notification, payload, sender) {
+  switch(notification) {
+    case "DOM_OBJECTS_CREATED":
+    var elem = document.getElementById("TEST")
+    elem.addEventListener("click", function(){
+    elem.innerHTML = "TEST2"
+    this.sendNotification('CHANGE_POSITIONS_DEFAULTS','')
+    }) 
+    this.sendNotification('CHANGE_POSITIONS', 
+    modules = {
+        'clock':{
+            visible: 'true',
+            position: 'top_left',
+        },
+    }
+);
+      break
+  }
+},
+})
